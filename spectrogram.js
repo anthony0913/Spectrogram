7340956b6d488cb655c9ead3858d1bf5c3eddd05
Anthony <44311909+anthony0625@users.noreply.github.com>
2023-02-19 20:47:12 +0900

    added amplitude multiplier

--- spectrogram.js
+++ spectrogram.js
@@ -7,7 +7,7 @@ const audioBufferSize = 2048;
 analyser.fftSize = 2048;
 const bufferLength = analyser.frequencyBinCount;
 const dataArray = new Uint8Array(bufferLength);
-const multiplier = 5;
+const multiplier = 1.2;
 let matrixSize = 100;
 let matrix = Array.from({ length: numBins }, () => new Array(matrixSize).fill(0));
 let spectrogramData = new Array(bufferLength).fill(0);
@@ -21,7 +21,7 @@ function drawHeatmap() {
   for (let i = 0; i < matrixSize - 1; i++) {
     for (let j = 0; j < numBins; j++) {
       const value = matrix[j][i];
-      const color = `rgb(${(1 - value) * 255}, ${(1 - value) * 255}, ${(1 - value) * 255})`;
+      const color = `rgb(${(1 - value * multiplier) * 255}, ${(1 - value * multiplier) * 255}, ${(1 - value * multiplier) * 255})`;
       const rectX = i * rectWidth;
       const rectY = j * rectHeight;
       ctx.fillStyle = color;
@@ -32,7 +32,7 @@ function drawHeatmap() {
   // Draw spectrogram data in last column
   const lastColumn = matrixSize - 1;
   for (let j = 0; j < numBins; j++) {
-    const value = spectrogramData[j] * 5; // scale all amplitude values by a constant factor of 5
+    const value = spectrogramData[j]; // scale all amplitude values by a constant factor of 5
     const r = 255 * Math.max(0, value - 0.5) * 2;
     const g = 255 * Math.abs(value - 0.5) * 2;
     const b = 255 * Math.max(0, 0.5 - value) * 2;
